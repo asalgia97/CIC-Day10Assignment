a55ef06307993d1bc56a7c735e2bf2f76bc53f99
anish <anishsalgia1997@gmail.com>
2020-12-10 23:11:22 +0530

    Add: Added code to calculate c+a/b

--- arithmeticSorting.sh
+++ arithmeticSorting.sh
@@ -5,3 +5,4 @@ read -p "Enter Second Number : " b
 read -p "Enter a Third Number : " c
 num=`echo | awk -v a=$a -v b=$b -v c=$c '{cal=a+b*c} END {print cal}'`
 num2=`echo | awk -v a=$a -v b=$b -v c=$c '{cal=a*b+c} END {print cal}'`
+num3=`echo | awk -v a=$a -v b=$b -v c=$c '{cal=c+a/b} END {print cal}'`
