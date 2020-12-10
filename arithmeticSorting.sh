c6014ffc810b2fd657027a265f076514dd730f40
anish <anishsalgia1997@gmail.com>
2020-12-10 23:34:21 +0530

    Add: Added code to store the calculations in dictionary

--- arithmeticSorting.sh
+++ arithmeticSorting.sh
@@ -1,9 +1,12 @@
-#!/bin/bash -x
+#!/usr/bin/bash -x
+
+declare -A calculateV
 
 read -p "Enter First Number : " a
 read -p "Enter Second Number : " b
 read -p "Enter a Third Number : " c
-num=`echo | awk -v a=$a -v b=$b -v c=$c '{cal=a+b*c} END {print cal}'`
-num2=`echo | awk -v a=$a -v b=$b -v c=$c '{cal=a*b+c} END {print cal}'`
-num3=`echo | awk -v a=$a -v b=$b -v c=$c '{cal=c+a/b} END {print cal}'`
-num4=`echo | awk -v a=$a -v b=$b -v c=$c '{cal=a%b+c} END {print cal}'`
+
+calculateV[1]=`echo | awk -v a=$a -v b=$b -v c=$c '{cal=a+b*c} END {print cal}'`
+calculateV[2]=`echo | awk -v a=$a -v b=$b -v c=$c '{cal=a*b+c} END {print cal}'`
+calculateV[3]=`echo | awk -v a=$a -v b=$b -v c=$c '{cal=c+a/b} END {print cal}'`
+calculateV[4]=`echo | awk -v a=$a -v b=$b -v c=$c '{cal=a%b+c} END {print cal}'`
