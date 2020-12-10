8ca48bbb5408026d7dc0bb9d8be009bf0e164ba5
anish <anishsalgia1997@gmail.com>
2020-12-10 23:40:50 +0530

    Add: Added code to read from Dictionary

--- arithmeticSorting.sh
+++ arithmeticSorting.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash -x
 
+arrayIndex=0
 declare -A calculateV
+declare -a Arr
 
 read -p "Enter First Number : " a
 read -p "Enter Second Number : " b
@@ -10,3 +12,9 @@ calculateV[1]=`echo | awk -v a=$a -v b=$b -v c=$c '{cal=a+b*c} END {print cal}'`
 calculateV[2]=`echo | awk -v a=$a -v b=$b -v c=$c '{cal=a*b+c} END {print cal}'`
 calculateV[3]=`echo | awk -v a=$a -v b=$b -v c=$c '{cal=c+a/b} END {print cal}'`
 calculateV[4]=`echo | awk -v a=$a -v b=$b -v c=$c '{cal=a%b+c} END {print cal}'`
+
+for value in "${calculateV[@]}"
+do
+	Arr[$arrayIndex]=$value
+	arrayIndex=$(( $arrayIndex + 1 ))
+done
