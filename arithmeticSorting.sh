3f090ae323191eac670d2c56654732a524c119ef
asalgia97 <37534229+asalgia97@users.noreply.github.com>
2020-12-21 21:45:04 +0530

    Update arithmeticSorting.sh

--- arithmeticSorting.sh
+++ arithmeticSorting.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/bash -x
-
 declare -A calculateV
-
-read -p "Enter First Number : " a
-read -p "Enter Second Number : " b
-read -p "Enter a Third Number : " c
-
+read -p "Enter First Number:"   a
+read -p "Enter Second Number:"  b
+read -p "Enter a Third Number:" c
 calculateV[1]=`echo | awk -v a=$a -v b=$b -v c=$c '{cal=a+b*c} END {print cal}'`
 calculateV[2]=`echo | awk -v a=$a -v b=$b -v c=$c '{cal=a*b+c} END {print cal}'`
 calculateV[3]=`echo | awk -v a=$a -v b=$b -v c=$c '{cal=c+a/b} END {print cal}'`
