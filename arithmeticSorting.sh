7ee724f7e771333afb349aad9170e090bc01e696
asalgia97 <37534229+asalgia97@users.noreply.github.com>
2020-12-21 21:42:19 +0530

    Update arithmeticSorting.sh

--- arithmeticSorting.sh
+++ arithmeticSorting.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -x
-
-read -p "Enter First Number : " a
-read -p "Enter Second Number : " b
-read -p "Enter a Third Number : " c
+read -p "Enter First Number:"   a
+read -p "Enter Second Number:"  b
+read -p "Enter a Third Number:" c
 num=`echo | awk -v a=$a -v b=$b -v c=$c '{cal=a+b*c} END {print cal}'`
